28a6ed1c67a4da8c7067f59814a178023a97cb1e
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-09-12 04:50:35 -0600

    Updated

---
+++ source/app.specs/CalculatorSpecs.cs
@@ -0,0 +1,11 @@
+﻿using Machine.Specifications;
+
+namespace app.specs
+{
+  [Subject(typeof(Calculator))]
+  public class when_adding_2_positive_numbers
+  {
+    It should_return_the_sum = () =>
+      Calculator.add(2, 3).ShouldEqual(5);
+  }
+}
