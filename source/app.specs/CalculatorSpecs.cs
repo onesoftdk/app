18091c9400c56e29991a29463bad3045a30b5dce
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-09-12 06:05:51 -0600

    Updated

--- source/app.specs/CalculatorSpecs.cs
+++ source/app.specs/CalculatorSpecs.cs
@@ -1,11 +1,37 @@
-﻿using Machine.Specifications;
+﻿using System;
+using Machine.Specifications;
+using developwithpassion.specifications.rhinomocks;
+using developwithpassion.specifications.extensions;
 
 namespace app.specs
 {
   [Subject(typeof(Calculator))]
-  public class when_adding_2_positive_numbers
+  public class CalculatorSpecs
   {
-    It should_return_the_sum = () =>
-      Calculator.add(2, 3).ShouldEqual(5);
+    public abstract class concern : Observes<Calculator>
+    {
+    }
+
+    public class when_adding_2_positive_numbers : concern
+    {
+      Because b = () =>
+        result = sut.add(2, 3);
+
+      It should_return_the_sum = () =>
+        result.ShouldEqual(5);
+
+      static int result;
+    }
+
+    public class when_attempting_to_add_a_negative_and_positive : concern
+    {
+      Because b = () =>
+        spec.catch_exception(() => sut.add(2, -3));
+
+      It should_throw_an_argument_exception = () =>
+        spec.exception_thrown.ShouldBeAn<ArgumentException>();
+
+      static int result;
+    }
   }
 }
