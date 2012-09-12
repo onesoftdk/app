9aaef543e6f25d6888bfca3b4b4f748618dba0a0
Henrik <hs@onesoft.dk>
2012-09-12 14:19:17 +0200

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using System.Data;
 
 namespace app
 {
   public class Calculator
   {
+      private IDbConnection _connection;
+
+      public Calculator() { }
+
+      public Calculator(IDbConnection connection)
+      {
+          _connection = connection;
+      }
+
     public int add(int first, int second)
     {
         if (first > 1 && second < 1)
@@ -12,7 +22,7 @@ namespace app
         if (second > 1 && first < 1)
             throw new ArgumentException();
 
-
+        _connection.Open();
 
         return first + second;
     }
