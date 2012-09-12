b5f67166ff5c9f7f5548cdabce86a3dba3431250
Josh <josh@jjmatthews.co.uk>
2012-09-12 13:29:05 +0100

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -3,22 +3,23 @@ using System.Data;
 
 namespace app
 {
-  public class Calculator
-  {
-    readonly IDbConnection connection;
-
-    public Calculator(IDbConnection connection)
+    public class Calculator
     {
-      this.connection = connection;
+        readonly IDbConnection connection;
+
+        public Calculator(IDbConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        public int add(int first, int second)
+        {
+            if (first < 0 || second < 0) throw new ArgumentException();
+
+            connection.Open();
+            var cmd = connection.CreateCommand();
+            cmd.ExecuteNonQuery();
+            return first + second;
+        }
     }
-
-    public int add(int first, int second)
-    {
-      if (first < 0 || second < 0) throw new ArgumentException();
-
-      connection.Open();
-
-      return first + second;
-    }
-  }
 }
