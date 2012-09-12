d92d514441c5c8af61198c09bb204b67bdcb2631
Morten <morten.rasch@coop.dk>
2012-09-12 14:08:39 +0200

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,9 +1,19 @@
-﻿namespace app
+﻿using System;
+
+namespace app
 {
   public class Calculator
   {
     public int add(int first, int second)
     {
+        if (first > 1 && second < 1)
+            throw new ArgumentException();
+
+        if (second > 1 && first < 1)
+            throw new ArgumentException();
+
+
+
         return first + second;
     }
   }
