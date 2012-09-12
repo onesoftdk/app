668c570005013158579d99384dcdf46dde87f1b6
Josh <josh@jjmatthews.co.uk>
2012-09-12 15:05:29 +0100

    Pushing new changes

--- source/app/web/BasicHandler.cs
+++ source/app/web/BasicHandler.cs
@@ -2,23 +2,25 @@
 
 namespace app.web
 {
-  public class BasicHandler : IHttpHandler
-  {
-    IProcessWebRequests front_controller;
-
-    public BasicHandler(IProcessWebRequests front_controller)
+    public class BasicHandler : IHttpHandler
     {
-      this.front_controller = front_controller;
-    }
+        IProcessWebRequests front_controller;
+        readonly ICreateRequests createRequests;
 
-    public void ProcessRequest(HttpContext context)
-    {
-      front_controller.process(context);
-    }
+        public BasicHandler(IProcessWebRequests front_controller, ICreateRequests createRequests)
+        {
+            this.front_controller = front_controller;
+            this.createRequests = createRequests;
+        }
 
-    public bool IsReusable
-    {
-      get { throw new System.NotImplementedException(); }
+        public void ProcessRequest(HttpContext context)
+        {
+            front_controller.process(createRequests.create_request_from(context));
+        }
+
+        public bool IsReusable
+        {
+            get { throw new System.NotImplementedException(); }
+        }
     }
-  }
 }
