c296980570d86f7a9bc11278d21e4c6185b330f7
Josh <josh@jjmatthews.co.uk>
2012-09-12 18:58:35 +0100

    Pushing new changes

--- source/app.specs/ViewTheMainDepartmentsInTheStoreSpecs.cs
+++ source/app.specs/ViewTheMainDepartmentsInTheStoreSpecs.cs
@@ -1,34 +1,34 @@
 ﻿using Machine.Specifications;
 using app.web.app.catalogbrowsing;
 using app.web.core;
+using developwithpassion.specifications.extensions;
 using developwithpassion.specifications.rhinomocks;
 
 namespace app.specs
 {
-  [Subject(typeof(ViewTheMainDepartmentsInTheStore))]
-  public class ViewTheMainDepartmentsInTheStoreSpecs
-  {
-    public abstract class concern : Observes<IImplementAFeature,
-                                      ViewTheMainDepartmentsInTheStore>
+    [Subject(typeof(ViewTheMainDepartmentsInTheStore))]
+    public class ViewTheMainDepartmentsInTheStoreSpecs
     {
+        public abstract class concern : Observes<IImplementAFeature,
+                                          ViewTheMainDepartmentsInTheStore>
+        {
+        }
+
+        public class when_run : concern
+        {
+            Establish c = () =>
+                              {
+                                  service_request = depends.on<IAmADepartmentServiceRequest>();
+                                  request = fake.an<IEncapsulateRequestDetails>();
+                              };
+
+            Because b = () =>
+              sut.process(request);
+
+            It should_get_the_main_departments_in_the_store = () => service_request.received(x => x.get_main_departments());
+
+            static IEncapsulateRequestDetails request;
+            static IAmADepartmentServiceRequest service_request;
+        }
     }
-
-    public class when_run : concern
-    {
-      Establish c = () =>
-      {
-        request = fake.an<IEncapsulateRequestDetails>();
-      };
-
-      Because b = () =>
-        sut.process(request);
-
-      It should_get_The_Main_departments_in_the_Store = () =>
-      {
-
-      };
-
-      static IEncapsulateRequestDetails request;
-    }
-  }
 }
---
+++ source/app/web/core/IAmADepartmentServiceRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace app.web.core
+{
+    public interface IAmADepartmentServiceRequest
+    {
+        void get_main_departments();
+    }
+}
