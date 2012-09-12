32e6be0b3a8979c4d66ee1f674a5176e777e7e61
Henrik <hs@onesoft.dk>
2012-09-12 14:07:43 +0200

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,9 +1,17 @@
-﻿namespace app
+﻿using System;
+
+namespace app
 {
   public class Calculator
   {
     public int add(int first, int second)
     {
+        if(first < 0)
+            throw new ArgumentException("can't be negative", "first");
+
+        if (second < 0)
+            throw new ArgumentException("can't be negative", "second");
+
         return first + second;
     }
   }
